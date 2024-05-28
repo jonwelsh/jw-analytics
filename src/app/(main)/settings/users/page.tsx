1524a1673d3eab267ae9dc68d09c868ee125f761
Jon <27546202+jonwelsh@users.noreply.github.com>
2024-05-28 23:00:12 +0100

    removed old

--- src/app/(main)/settings/users/page.tsx
+++
@@ -1,9 +0,0 @@
-import UsersDataTable from './UsersDataTable';
-import { Metadata } from 'next';
-
-export default function () {
-  return <UsersDataTable />;
-}
-export const metadata: Metadata = {
-  title: 'Users | umami',
-};
